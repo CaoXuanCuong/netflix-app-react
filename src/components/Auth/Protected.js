f220a87ed8fa5efaa793db99031ed4e5beb8ff89
CaoXuanCuong <cuong.2992000@gmail.com>
2022-10-16 19:25:29 +0700

    Fix UI loading auth

--- src/components/Auth/Protected.js
+++ src/components/Auth/Protected.js
@@ -1,19 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '~/context/AuthContext';
 
-const Protected = ({children}) => {
+const Protected = ({ children }) => {
     const {
         authState: { authLoading, isAuthenticated },
     } = useContext(AuthContext);
 
-    if (authLoading) return <div className='spinner-container'>Loading...</div>
-		
-    return isAuthenticated ? (
-        <>{children}</>
-    ) : (
-        <Navigate to="/" />
-    );
-}
+    if (authLoading) {
+        return (
+            <div className="spinner-container">
+                <div className="loading">
+                    <i className="bx bx-loader-alt"></i>
+                </div>
+            </div>
+        );
+    }
 
-export default Protected
+    return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
+};
+
+export default Protected;
--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -1,32 +1,39 @@
-import React, { useContext } from 'react'
-import { Link, Navigate, Outlet } from 'react-router-dom'
-import images from '~/assets/images'
+import React, { useContext } from 'react';
+import { Link, Navigate, Outlet } from 'react-router-dom';
+import images from '~/assets/images';
 import { AuthContext } from '~/context/AuthContext';
 import config from '~/configs';
 
 const AuthLayout = () => {
-  const {
-    authState: { authLoading, isAuthenticated },
-  } = useContext(AuthContext);
+    const {
+        authState: { authLoading, isAuthenticated },
+    } = useContext(AuthContext);
 
-  if (authLoading) return <div>Loading...</div>
-  return (
-    <>
-      {!isAuthenticated ? (
-        <div className='auth-page'>
-            <Link to={config.routes.home}>
-              <img width="250px" className='mb-2' alt='Logo' src={images.netFlix} />
-            </Link>
-            <Outlet />
-            <div className='mt-5 fs-14 text-center'>
-              © NetFlix - Watch Online English Subtitles Movies Tv Series
+    if (authLoading)
+        return (
+            <div className="spinner-container">
+                <div className="loading">
+                    <i className="bx bx-loader-alt"></i>
+                </div>
             </div>
-        </div>
-      ) : (
-        <Navigate to={config.routes.home} />
-      )}
-    </>
-  )
-}
+        );
+    return (
+        <>
+            {!isAuthenticated ? (
+                <div className="auth-page">
+                    <Link to={config.routes.home}>
+                        <img width="250px" className="mb-2" alt="Logo" src={images.netFlix} />
+                    </Link>
+                    <Outlet />
+                    <div className="mt-5 fs-14 text-center">
+                        © NetFlix - Watch Online English Subtitles Movies Tv Series
+                    </div>
+                </div>
+            ) : (
+                <Navigate to={config.routes.home} />
+            )}
+        </>
+    );
+};
 
-export default AuthLayout
+export default AuthLayout;
